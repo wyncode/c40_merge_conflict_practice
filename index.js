41ae40fc5d91c12e81785d0a33f3ef3d1c32501b
Cece Claridge <cece.claridge@gmail.com>
2020-11-15 11:42:32 -0500

    Add Lizzo's poem and change background color

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ const c40poems = [
     poem:
       "Roses are red \n Violets are blue \n Sunflowers are yellow \n I bet you were expecting something romantic but no -- these are just gardening facts.",
   },
-
-  // YOUR CODE HERE: Add an object containing two key value pairs: author and poem. Set the author property as equal to a string with your first name. Set the poem property as equal to a short "roses are red" poem. Use the new line character as in the example when you would like a line break.
-
   {
     author: "Cece",
     poem:
@@ -16,6 +13,14 @@ const c40poems = [
     author: "Cece",
     poem:
       "Roses are red \n Violets are blue \n Jerry Seinfeld is considering \n Making Bee Movie 2",
+  },
+
+  // YOUR CODE HERE: Add an object containing two key value pairs: author and poem. Set the author property as equal to a string with your first name. Set the poem property as equal to a short "roses are red" poem. Use the new line character as in the example when you would like a line break.
+
+  {
+    author: "Lizzo",
+    poem:
+      "Roses are red \n Sometimes life isn't fair \n Shampoo press \n Get you outta my hair",
   },
 ];
 
