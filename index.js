4d01120bd565dfed7f7a1bdf56365d055fb27524
Cece Claridge <cece.claridge@gmail.com>
2020-11-15 10:36:10 -0500

    add first roses are red poem

---
+++ index.js
@@ -0,0 +1,32 @@
+const c40poems = [
+  {
+    author: "Cece",
+    poem:
+      "Roses are red \n Violets are blue \n Sunflowers are yellow \n I bet you were expecting something romantic but no -- these are just gardening facts.",
+  },
+
+  // YOUR CODE HERE: Add an object containing two key value pairs: author and poem. Set the author property as equal to a string with your first name. Set the poem property as equal to a short "roses are red" poem. Use the new line character as in the example when you would like a line break.
+];
+
+let displayPoems = (array) => {
+  let html = "";
+  let poemDiv = document.getElementById("poems");
+  let poemArray = array;
+
+  poemArray = array.map((poem) => {
+    html += `
+    <div class="card">
+        <div class="author card-header">
+            Author: ${poem.author}
+        </div>
+        <div class="single-poem card-body">
+            ${poem.poem}
+        </div>
+    </div>
+    `;
+  });
+
+  poemDiv.innerHTML = html;
+};
+
+displayPoems(c40poems);
