3c4827ce8e95783f3d9e832cd3e44f7b5c646bd4
Cece Claridge <cece.claridge@gmail.com>
2020-11-15 11:55:47 -0500

    Add Cardi's poem and background color

--- index.js
+++ index.js
@@ -15,6 +15,11 @@ const c40poems = [
       "Roses are red \n Violets are blue \n Jerry Seinfeld is considering \n Making Bee Movie 2",
   },
   // YOUR CODE HERE: Add an object containing two key value pairs: author and poem. Set the author property as equal to a string with your first name. Set the poem property as equal to a short "roses are red" poem. Use the new line character as in the example when you would like a line break.
+  {
+    author: "Cardi",
+    poem:
+      "Roses are red \n Just like my Gucci socks \n Haters who doubt me \n Y'all can kick rocks",
+  },
 ];
 
 let displayPoems = (array) => {
