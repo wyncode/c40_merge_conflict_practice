5b9e55b8428f0f8ce85fbca80475da84de6f695b
mayorgalu <mayorgalu@estrategy.tech>
2020-11-16 11:52:36 -0400

    Added Phoebe Lyrics

--- index.js
+++ index.js
@@ -15,7 +15,11 @@ const c40poems = [
       "Roses are red \n Violets are blue \n Jerry Seinfeld is considering \n Making Bee Movie 2",
   },
 
-  // YOUR CODE HERE: Add an object containing two key value pairs: author and poem. Set the author property as equal to a string with your first name. Set the poem property as equal to a short "roses are red" poem. Use the new line character as in the example when you would like a line break.
+  {
+    author: "Phoebe Bridgers",
+    poem:
+      "Roses are red \n Haunting voices in the smoky lyrics \n Friday I'm in love",
+  },
 
   {
     author: "Lizzo",
